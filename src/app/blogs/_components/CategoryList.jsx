ae21dbb909557ddb7e8089d95091147daab7d2b2
reza <reza.hashemi19@gmail.com>
2024-11-12 14:40:41 +0330

    first_Commit

--- src/app/blogs/_components/CategoryList.jsx
+++ src/app/blogs/_components/CategoryList.jsx
@@ -8,5 +8,6 @@ export default async function CategoryList() {
 
   return (
     <div>CategoryList</div>
+    
   )
 }
--- src/app/blogs/layout.jsx
+++ src/app/blogs/layout.jsx
@@ -1,10 +1,10 @@
-
+import CategoryList from "./_components/CategoryList"
 export default function Layout({children}) {
   return (
   <div className="grid grid-cols-12 h-screen">
     {/* منوی سمت راست */}
     <div className="col-span-2 bg-gray-800 text-white p-4">
-      <p>منوی سمت راست</p>
+     <CategoryList/>
     </div>
     {/* بخش محتوای سمت چپ */}
     <div className="col-span-8 bg-gray-100 p-6">
